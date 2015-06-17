b1114613635dcc03592b2e9ce8961b4c975aedbe
jsukup <jesukup@gmail.com>
2015-06-16 22:12:46 -0500

    Fixed map and added GeoJSON polygons

--- ChiFood.R
+++ ChiFood.R
@@ -1,11 +1,14 @@
-setwd("C:/Users/sukujo01/Downloads")
+#setwd("C:/Users/sukujo01/Downloads")# Work laptop directory
+
 
 library(lubridate)
-library(choroplethr)
+library(leaflet)
 library(dplyr)
+library(shiny)
+library(jsonlite)
 
 ##Data Processing
-data <- read.csv("Food_Inspections.csv", header = TRUE, stringsAsFactors = FALSE)# Read in data
+data <- read.csv("C:/Users/John E Sukup III/Downloads/Food_Inspections.csv", header = TRUE, stringsAsFactors = FALSE)# Read in data
 
 names(data) <- tolower(names(data))# Lower case varnames
 data$inspection.date <- parse_date_time(data$inspection.date, "mdy")# Convert to POSIX date variable
@@ -36,3 +39,14 @@ zips <- chidat %>%
         group_by(as.character(zip)) %>%
         summarise(mean(as.numeric(risk)))
 names(zips) <- c("region","value")
+
+geojson <- readLines("https://raw.githubusercontent.com/smartchicago/chicago-atlas/master/db/import/zipcodes.geojson", warn = FALSE) %>%
+    paste(collapse = "\n") %>%
+    fromJSON(simplifyVector = FALSE)
+
+map <- leaflet() %>% 
+    addTiles() %>% 
+    addGeoJSON(geojson) %>%
+    setMaxBounds(-87.9353, 42.0003, -87.5171, 41.6746) %>% 
+    setView(-87, 42, zoom = 11)
+
