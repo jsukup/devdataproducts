62590cba87a28ea1c0b2ce81a02c9492790a2eda
jsukup <jesukup@gmail.com>
2015-06-20 22:44:47 -0500

    First working version

--- app.R
+++ app.R
@@ -6,55 +6,42 @@ library(jsonlite)
 
 ## Shiny app
 load(file = "data.RData")# Load RData
+data <- data %>% 
+    group_by(dba.name,
+             latitude,
+             longitude,
+             zip) %>% 
+    summarise(risk = round(mean(as.integer(risk)))) 
+
 
 # Leaflet map
-# # Zip code filter
+## Zip code filter
 ui <- fluidPage(
-        leafletOutput("map"),
-        selectInput("zipcodes", "Select Zip Code", choices = c(data$zip), selected = "60601")
-        )
-
+    leafletOutput("chimap"),
+    selectInput("zipcodes", "Select Zip Code", choices = sort(data$zip), selected = "60601")
+)
 
 server <- function(input, output){
-    zip <- eventReactive(input$zipcodes, {
-        filter(data, zip == input) %>% 
-        group_by(dba.name, 
-                 latitude, 
-                 longitude) %>% 
-        summarise(risk = round(mean(as.integer(risk))))
-        }
-)
+    data <- data
+    getData <- reactive({data[data$zip == input$zipcodes,]})
     
-    color <- colorFactor(c("red","yellow","green"), domain = zip$risk)# Create color palette
-
-    
-    output$map <- renderLeaflet({
-        leaflet(zip) %>% 
+    output$chimap <- renderLeaflet({
+        leaflet(getData()) %>% 
         addProviderTiles("Stamen.Toner") %>% 
         addCircleMarkers(lat = ~latitude, 
                          lng = ~longitude, 
                          popup = ~dba.name, 
-                         color = ~color(risk), 
+ # Create color palette
                          fillOpacity = .3) %>% 
         mapOptions(zoomToLimits = "always") %>% 
-        addLegend("topright", 
-                  color = c("red","yellow","green"), 
-                  labels = c("High Risk","Medium Risk","Low Risk"), 
+        addLegend("topright",
+                  color = c("red","yellow","green"),
+                  labels = c("High Risk","Medium Risk","Low Risk"),
                   values = ~risk,
                   title = "Risk Level for Chicago Restaurants",
                   opacity = .7)
-            }
-  )
+    }
+    )
 }
-    
+  
 shinyApp(ui, server)        
-
-
-
-
-
-
-
-
-
-
